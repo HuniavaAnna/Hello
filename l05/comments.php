aec2406ceb006beb124389558bf5e42c5ddc7d8b
HuniavaAnna <antusy16@gmail.com>
2023-08-31 19:19:19 +0300

    L05: Added comments examples

---
+++ l05/comments.php
@@ -0,0 +1,21 @@
+<?php
+
+// echo 'test 01', PHP_EOL;
+# echo 'test 02', PHP_EOL;
+echo 'test 03', PHP_EOL;
+/*
+echo 'test 04', PHP_EOL;
+echo 'test 05', PHP_EOL;
+*/
+echo 'test 06', PHP_EOL;
+
+/**
+ * Shows on example of API-style comment
+ * @param string $testA
+ * @param int $testB
+ * @return string
+ */
+function testSMTH($testA, $testB)
+{
+    return 'TEST';
+    }
