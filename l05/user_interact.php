e9cc9d621f3a0765b7899bc3bb96ca0e09cb117c
HuniavaAnna <antusy16@gmail.com>
2023-09-01 19:11:06 +0300

    L05: Added user interact console script

---
+++ l05/user_interact.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = readline('Enter your name: ');
+$age = readline('Enter your age: ');
+$password = readline('Enter your password: ');
+
+echo "Hello {$name}, {$age} years old. Don't forgot your password: {$password}", PHP_EOL;
+
+
+
