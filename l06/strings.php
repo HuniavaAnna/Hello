32146e5d9b2acddcc06d5ea67ed4fd3ca8aac54d
HuniavaAnna <antusy16@gmail.com>
2023-09-06 19:16:58 +0300

    L06: Added string formats demonstration

--- l06/strings.php
+++ l06/strings.php
@@ -10,3 +10,19 @@ print PHP_EOL;
 $s1 = print 'test 123';
 var_dump($s1);
 
+$name = 'Anna';
+echo 'Hello $name\n', PHP_EOL;
+echo "Hello, user name \"{$name}\"\n";
+
+$nowdoc = <<<'TEXT'
+<script type="text/javascript">
+    alert(1);
+TEXT;
+echo $nowdoc, PHP_EOL;
+
+$heredoc = <<<TEXT
+<script type="text/javascript">
+    alert(1);  
+TEXT;
+echo $heredoc, PHP_EOL;
+
