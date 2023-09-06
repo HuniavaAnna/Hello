7156476d1f990ca3a360ac88488b2a0b2b098c1d
HuniavaAnna <antusy16@gmail.com>
2023-09-06 20:23:05 +0300

    L06: Added increments demonstration

--- l06/math.php
+++ l06/math.php
@@ -19,4 +19,18 @@ $resvlt +=10;
 $resvlt /=2;
 echo $resvlt, PHP_EOL;
 
+// increment, when ++ do in +1. decrement, when -- do it -1.
+$number = 5;
+$number++;
+$number++;
+$number--;
+echo $number, PHP_EOL;
+
+$number2 = 5;
+++$number2;
+--$number2;
+echo ++$number2, PHP_EOL;
+
+$number3 = 5;
+echo $number3++ + ++$number3, PHP_EOL;
 
