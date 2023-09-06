e21791c3d2a93a6d8b44102a4fe4c64b5e66cbea
HuniavaAnna <antusy16@gmail.com>
2023-09-06 20:09:58 +0300

    L06: Added math operators demonstration

---
+++ l06/math.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 1 + 2, PHP_EOL;
+echo 1 * 2, PHP_EOL;
+echo 1 - 2, PHP_EOL;
+echo 1 / 2, PHP_EOL;
+
+echo 3 % 2, PHP_EOL;
+echo 3 % 5, PHP_EOL;
+echo 8 % 3, PHP_EOL;
+
+echo 3 ** 2, PHP_EOL;
+echo 3 ** 5, PHP_EOL;
+
+$resvlt = 0;
+// $resvlt = $resvlt + 2;
+$resvlt +=2;
+$resvlt +=10;
+$resvlt /=2;
+echo $resvlt, PHP_EOL;
+
+
