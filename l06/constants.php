3ca03bff954750c18f4adc4521f7b20349069c84
HuniavaAnna <antusy16@gmail.com>
2023-09-06 19:44:48 +0300

    L06: Added constants demonstration

---
+++ l06/constants.php
@@ -0,0 +1,13 @@
+<?php
+// Can be only one
+define('APP_VERSION', '3.0.1');
+// define('APP_VERSION', '3.0.2'); is not working.
+echo APP_VERSION, PHP_EOL;
+
+const POWER = '220V';
+// const POWER = '221V'; is not working.
+echo POWER, PHP_EOL;
+
+const MY_COEFFICIENT = 3.1;
+$salary = 2000;
+echo $salary * MY_COEFFICIENT, PHP_EOL;
