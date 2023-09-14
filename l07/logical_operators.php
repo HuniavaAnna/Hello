d24a5a0b2dda71d95475abbae1348cc9d2fd31a4
HuniavaAnna <antusy16@gmail.com>
2023-09-14 18:21:16 +0300

    L06: Added logical operators demonstration

---
+++ l07/logical_operators.php
@@ -0,0 +1,25 @@
+<?php
+
+var_dump(1 == 3);
+var_dump(1 != 1);
+var_dump('test' == 'test');
+
+var_dump(1 === '1');
+var_dump(0 === false);
+var_dump(0 == false);
+
+var_dump(1 > 3);
+var_dump('2020-05-20' > '2020-05-01');
+var_dump(1 >= 1);
+var_dump(1 > 1);
+
+//$minSalary = 350;
+//$maxSalary = 700;
+//$salary = random_int(300, 1000);
+
+////and
+//var_dump($salary, $salary >= $minSalary && $salary <= $maxSalary);
+////or
+//var_dump($salary, $salary >= $minSalary || $salary <= $maxSalary);
+//
+//var_dump($salary, $salary >= $minSalary && $salary <= $maxSalary && $salary != 666);
