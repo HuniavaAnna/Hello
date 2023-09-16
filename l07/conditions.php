607e4b89d9edc54a9f1f4b5a922168d75456590f
HuniavaAnna <antusy16@gmail.com>
2023-09-16 13:29:25 +0300

    L06: Added homework

--- l07/conditions.php
+++ l07/conditions.php
@@ -13,15 +13,15 @@ echo $age >= 18 ? 'OK' : 'FAIL';
 echo PHP_EOL;
 
 // Homework
-// print - 123
-//$x = ;
-//if ($x == 1) {
-//    echo 1;
-//}
-//if ($x == 2) {
-//    echo 2;
-//}
-//if ($x == 3) {
-//    echo 3;
-//}
+// print - 123;
+$x = true;
+if ($x == 1) {
+    echo 1;
+}
+if ($x == 2) {
+    echo 2;
+}
+if ($x == 3) {
+    echo 3;
+}
 // 2 - calculator
