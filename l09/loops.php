a51143b43603f35c703931d20d79f86be182f0d5
HuniavaAnna <antusy16@gmail.com>
2023-09-19 18:48:33 +0300

    L06: Added for example

--- l09/loops.php
+++ l09/loops.php
@@ -1,5 +1,28 @@
 <?php
 
-for ($i = 0; $i < 5; $i++) {
-    echo $i, '<br>';
+$eol = php_sapi_name() === 'cli' ? PHP_EOL : '<br>';
+
+for ($i = 0; $i <10; $i++) {
+    echo $i, $eol;
 }
+
+for ( ; ; ) {
+    $rand = random_int(1,5);
+    echo $rand, $eol;
+    if ($rand === 3) {
+        break;
+    }
+    sleep(1);
+}
+
+echo $eol;
+
+$array = ['test', 'qwerty', '1234567'];
+$count = count($array);
+for ($i = 0; $i < $count; $i++) {
+    echo $array[$i], $eol;
+}
+
+echo $eol;
+
+
