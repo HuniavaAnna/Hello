f140ea4b70ca3164233fa7f5cfd8a20ae1fbee07
HuniavaAnna <antusy16@gmail.com>
2023-09-24 16:45:58 +0300

    L06: Added loops in homework, Will done

--- l09/menu_website.php
+++ l09/menu_website.php
@@ -1,28 +1,30 @@
 <?php
 
-$menu = [
-    "/link1",
-    "/link2",
-        [
-            "/link3_1",
-            "/link3_1",
+$menuItems = [
+    'Головна' => "/link1",
+    'Контакти' => "/link2",
+    'Товари' =>  [
+        'Книги' =>  "/link3_1",
+        'Комікси' =>  "/link3_1",
         ]
     ];
 
-foreach ($menu as $item) {
+echo '<ul>';
+foreach ($menuItems as $label => $link) {
     echo '<li>';
+    if (is_array($link)) {
+        echo $label;
+        echo '<ul>';
+        foreach ($link as $subLabel => $subLink) {
+            echo '<li><a href="' . $subLink . '">' . $subLabel . '</a></li>';
+        }
+        echo '</ul>';
+    } else {
+        echo '<a href="' . $link . '">' . $label . '</a>';
+    }
+    echo '</li>';
 }
+echo '</ul>';
 ?>
- <ul>
-      <li><a href="/link1">Головна</a></li>
-      <li><a href="/link2">Про нас</a></li>
-      <li>
-          Товари
-          <ul>
-              <li><a href="/link3_1">Книги</a></li>
-              <li><a href="/link3_2">Комікси</a></li>
-          </ul>
-      </li>
- </ul>
 
 
